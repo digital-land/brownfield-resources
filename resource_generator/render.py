09a321a0947380e8c93ebda8649175666bfeafd6
Colm Britton <colmjude@gmail.com>
2019-12-10 16:52:35 +0000

    Add govuk-jinja-components library

--- resource_generator/render.py
+++ resource_generator/render.py
@@ -2,17 +2,29 @@ import jinja2
 
 from resource_generator.filters import pluralise
 
-loader = jinja2.FileSystemLoader(searchpath="./resource_generator/templates")
-env = jinja2.Environment(loader=loader)
+
+# set up paths to where the templates are
+def register_templates():
+    multi_loader = jinja2.ChoiceLoader([
+        jinja2.FileSystemLoader(searchpath="./resource_generator/templates"),
+        jinja2.PrefixLoader({
+            'govuk-jinja-components': jinja2.PackageLoader('govuk-jinja-components')
+        })
+    ])
+    return jinja2.Environment(loader=multi_loader)
 
 
+# register filters with jinja context
 def register_filters(env):
     env.filters["pluralise"] = pluralise
 
 
+# jinja setup
+env = register_templates()
 register_filters(env)
 
 
 def render_html(data):
     test_template = env.get_template("test.html")
-    return test_template.render(report=data)
+    html = test_template.render(report=data)
+    return html
