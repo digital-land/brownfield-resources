ce422e0da814c0106f20498231f5f7bbb85c30f2
Colm Britton <colmjude@gmail.com>
2020-01-20 14:58:05 +0000

    Add last updated date and latest css to report page

--- resource_generator/report_page.py
+++ resource_generator/report_page.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import jinja2
+import datetime
 
 from collection import CollectionIndex, heat_map_data
 from render import register_templates
 
 from filters import colour_for_count
 
-static_folder = "https://brownfield-sites-validator.herokuapp.com/static"
+static_folder = "https://digital-land-design.herokuapp.com/static"
 
 # register filters with jinja context
 def register_filters(env):
@@ -24,12 +25,13 @@ register_filters(env)
 
 report_template = env.get_template("report.html")
 
-weeks = heat_map_data('2019-11-01')
+weeks = heat_map_data('2019-09-01')
 
 with open("tmp/report.html", "w") as f:
     f.write(report_template.render(
         static_folder=static_folder,
         mappings=ind.mappings,
         orgs_by_links=orgs_by_links,
-        heat_map=weeks)
+        heat_map=weeks,
+        today=datetime.datetime.today())
     )
