3f5328063778d8cdb669dedce95879c9474bcd3e
Colm Britton <colmjude@gmail.com>
2020-01-21 16:17:18 +0000

    Func to return array of date strings

--- resource_generator/utils.py
+++ resource_generator/utils.py
@@ -1,5 +1,20 @@
 import os
 
+from resource_generator.collection import previous_day
+
 def extract_file_name(file):
     base = os.path.basename(file)
     return os.path.splitext(base)[0]
+
+
+def day_strs_between(start_date, end_date):
+    day_strs = []
+
+    day_str = end_date
+    day_before_start = previous_day(start_date)
+
+    while day_str != day_before_start:
+        day_strs.append(day_str)
+        day_str = previous_day(day_str)
+
+    return day_strs
