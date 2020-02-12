6496eba734c876863806340de4597a8ada878d29
Colm Britton <colmjude@gmail.com>
2020-02-12 10:33:20 +0000

    Count number of sites that have permission in principle

--- resource_generator/data_analyser.py
+++ resource_generator/data_analyser.py
@@ -7,22 +7,27 @@ class DataAnalyser():
         super().__init__()
         self.data = data
 
+    def count_pip(self):
+        permission_types = [x["PermissionType"] for x in self.data]
+        return permission_types.count("permission in principle")
+
     def row_count(self):
         return len(self.data)
 
     def sum_max_dwellings(self):
-        return sum([x['maximum-net-dwellings'] for x in self.data])
+        return sum([x['NetDwellingsRangeTo'] for x in self.data])
 
     def sum_min_dwellings(self):
-        return sum([x['minimum-net-dwellings'] for x in self.data])
+        return sum([x['NetDwellingsRangeFrom'] for x in self.data])
 
     def total_hectares(self):
-        return sum([x['hectares'] for x in self.data])
+        return sum([x['Hectares'] for x in self.data])
 
     def summary(self):
         return {
             "total": self.row_count(),
             "hectares": self.total_hectares(),
             "max_dwellings": self.sum_max_dwellings(),
-            "min_dwellings": self.sum_min_dwellings()
+            "min_dwellings": self.sum_min_dwellings(),
+            "permission_in_principle": self.count_pip()
         }
