7d4e9f6afa0e89131417f6658f1d754b47decfbc
Colm Britton <colmjude@gmail.com>
2020-02-20 09:11:02 +0000

    Generate all check pages in one go

--- resource_generator/check_per_org.py
+++ resource_generator/check_per_org.py
@@ -11,7 +11,7 @@ from resource_generator.filters import readable_date, map_org_code_to_name
 ind = CollectionIndex()
 
 # jinja setup
-renderer = Renderer("https://digital-land-design.herokuapp.com/static")
+renderer = Renderer("https://digital-land-design.herokuapp.com/static", dist_dir="../dataset/docs")
 #renderer.register_filter("readable_date", readable_date)
 renderer.register_filter("map_org_code_to_name", map_org_code_to_name)
 
@@ -28,6 +28,11 @@ def sortResources(resource_list):
     return sorted(_sorted, key=lambda x: x['start_date'])
 
 
+def generate_all_check_pages():
+    for org in ind.mappings['organisation'].keys():
+        generate_org_check_page(org)
+
+
 # generate a page for a given resource
 def generate_org_check_page(org_id):
     resources = ind.get_resources_for_org(org_id)
@@ -36,11 +41,14 @@ def generate_org_check_page(org_id):
     if len(_sorted_resources) > 1:
         other_resources = _sorted_resources[:-1]
 
+    # switch local-authority-eng:HGO to local-authority-eng/HGO
+    curried_id = org_id.replace(":", "/")
+
     # render the page
     try:
         renderer.render_page(
             "check-page-per-org.html",
-            f"dataset/brownfield-land/organisation/{org_id}/check.html",
+            f"brownfield-land/organisation/{curried_id}/check.html",
             organisation=org_id,
             ind=ind,
             latest_resource=_sorted_resources[-1],
@@ -57,8 +65,12 @@ if __name__ == '__main__':
     # default resource hash
     org_id = "local-authority-eng:NNO"
     if len(sys.argv) > 1:
-        org_id = sys.argv[1]
-        print(f"Generate check data page for resource: {sys.argv[1]}")
+        if sys.argv[1] == "--all":
+            generate_all_check_pages()
+            sys.exit(0)
+        else:
+            org_id = sys.argv[1]
+            print(f"Generate check data page for resource: {sys.argv[1]}")
     else:
         print(f"Generate check data page for default resource: {org_id}")
     generate_org_check_page(org_id)
