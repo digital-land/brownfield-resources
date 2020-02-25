482624eb7118ddf7b71d3197bb8eeaffd4962945
Colm Britton <colmjude@gmail.com>
2020-02-25 20:48:03 +0000

    Highlight how many orgs are referenced in the resource

--- resource_generator/data_analyser.py
+++ resource_generator/data_analyser.py
@@ -22,6 +22,9 @@ class DataAnalyser():
     def row_count(self):
         return len(self.data)
 
+    def org_uris(self):
+        return [row['OrganisationURI'] for row in self.data]
+
     def sum_max_dwellings(self):
         return sum([x['NetDwellingsRangeTo'] for x in self.data if isinstance(x['NetDwellingsRangeTo'], Number)])
 
@@ -39,5 +42,6 @@ class DataAnalyser():
             "hectares": self.total_hectares(),
             "max_dwellings": self.sum_max_dwellings(),
             "min_dwellings": self.sum_min_dwellings(),
-            "permission_in_principle": self.count_pip()
+            "permission_in_principle": self.count_pip(),
+            "unique_organisation_uris": len(set(self.org_uris()))
         }
