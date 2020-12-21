af63480219335dc67a50ce577e045d53b3866a58
Colm Britton <colmjude@gmail.com>
2020-12-21 14:54:39 +0000

    fix the check your latest register page

--- resource_generator/check_per_org.py
+++ resource_generator/check_per_org.py
@@ -4,65 +4,58 @@ import sys
 import json
 
 from resource_generator.renderer import Renderer
-from resource_generator.collection import CollectionIndex
-from resource_generator.filters import readable_date, map_org_code_to_name, pluralise
-
-# need collection
-ind = CollectionIndex()
+from resource_generator.filters import map_org_code_to_name, pluralise
+from resource_generator.utils import read_csv
 
 # jinja setup
 renderer = Renderer(dist_dir="../dataset/docs")
-#renderer.register_filter("readable_date", readable_date)
 renderer.register_filter("map_org_code_to_name", map_org_code_to_name)
 renderer.register_filter("pluralise", pluralise)
 
 
-def sortResources(resource_list):
-    _sorted = []
-    for r in resource_list:
-        last_collected = ind.date_resource_last_collected(r)
-        _sorted.append({
-            'resource': r,
-            'last_collected': last_collected,
-            'start_date': ind.date_resource_first_collected(r)
-        })
-    return sorted(_sorted, key=lambda x: x['start_date'])
+def index_by_org(resources):
+    idx = {}
+    for resource in resources:
+        orgs = resource["organisations"].split(";")
+        for org in orgs:
+            idx.setdefault(org, {"resource": []})
+            idx[org]["resource"].append(resource)
+    return idx
 
 
 def generate_all_check_pages():
-    for org in ind.mappings['organisation'].keys():
-        generate_org_check_page(org)
+    # get list of all resources
+    all_resources = read_csv("data/resource.csv")
+    index_resources = index_by_org(all_resources)
+    for org in index_resources.keys():
+        index_resources[org]["resource"].sort(key=lambda x: x["start-date"])
+        generate_org_check_page(org, index_resources.get(org)["resource"])
 
 
 # generate a page for a given resource
-def generate_org_check_page(org_id):
-    resources = ind.get_resources_for_org(org_id)
-    _sorted_resources = sortResources(resources)
-    other_resources = []
-    if len(_sorted_resources) > 1:
-        other_resources = _sorted_resources[:-1]
+def generate_org_check_page(organisation, resources):
 
     # switch local-authority-eng:HGO to local-authority-eng/HGO
-    curried_id = org_id.replace(":", "/")
+    curried_id = organisation.replace(":", "/")
 
     # render the page
     try:
         renderer.render_page(
             "check-page-per-org.html",
             f"brownfield-land/organisation/{curried_id}/check.html",
-            organisation=org_id,
-            ind=ind,
-            latest_resource=_sorted_resources[-1],
-            other_resources=other_resources)
-        print(f"SUCCESS: {org_id}")
+            organisation=organisation,
+            latest_resource=resources[-1],
+            other_resources=resources[:-1] if len(resources) > 1 else [],
+        )
+        print(f"SUCCESS: {organisation}")
         return True
     except Exception as e:
-        print(f"Failed for: {org_id}")
+        print(f"Failed for: {organisation}")
         print(repr(e))
         return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # default resource hash
     org_id = "local-authority-eng:NNO"
     if len(sys.argv) > 1:
